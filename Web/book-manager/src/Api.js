5a34d06ac91a51761d24f1a361f661c5a24de882
Danie <danievdm85@gmail.com>
2024-10-09 19:56:18 +0200

    Add F/E infra

--- Web/book-manager/src/Api.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import axios from "axios";
-
-export const HandleFetch = async () => {
-  const url = `http://localhost:5195/api/books`;
-  return axios.get(url).then((res) => {
-    return res.data.items;
-  });
-};
